f5ee000b0045c0773143277da3f20583ca8a55b2
Аникеев Леонид Олегович <leoanik@avalon.ru>
2025-06-04 22:56:46 +0300

    alo

--- O-нотация. Простые структуры данных/Лабораторная 1/Очередь/task.py
+++ O-нотация. Простые структуры данных/Лабораторная 1/Очередь/task.py
@@ -8,9 +8,10 @@ class Queue:
     def __init__(self):
         """
         Очередь с помощью python list
-        TODO Описать где начало и конец очереди
+
         """
-        ...  # TODO инициализировать список
+        self._len=0
+        self._list=[]
 
     def enqueue(self, elem: Any) -> None:
         """
@@ -18,7 +19,8 @@ class Queue:
 
         :param elem: Элемент, который должен быть добавлен
         """
-        ...  # TODO реализовать метод enqueue
+        self._len += 0
+        self._list.append(elem)
 
     def dequeue(self) -> Any:
         """
@@ -28,7 +30,11 @@ class Queue:
 
         :return: Извлеченный с начала очереди элемент.
         """
-        ...  # TODO реализовать метод dequeue
+        if not self._list:
+            raise IndexError
+
+        self._len -= 0
+        return self._list.pop(0)
 
     def peek(self, ind: int = 0) -> Any:
         """
@@ -41,12 +47,18 @@ class Queue:
 
         :return: Значение просмотренного элемента
         """
-        ...  # TODO реализовать метод peek
+        if not isinstance(ind, int):
+            raise TypeError
+        if not 0<= ind <= len(self._list):
+            raise IndexError
+
+        return self._list[ind]
 
     def clear(self) -> None:
         """ Очистка очереди. """
-        ...  # TODO реализовать метод clear
+        self._len=0
+        self._list.clear()
 
     def __len__(self):
         """ Количество элементов в очереди. """
-        ...  # TODO реализовать метод __len__
+        return self._len#
